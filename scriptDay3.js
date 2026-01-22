498c6c1eb695edc8c2079ec4317a0464ecb7c25f
Pallavi Bajpai <pallavibajpai2809@gmail.com>
2026-01-22 13:17:08 +0530

    first commit

---
+++ scriptDay3.js
@@ -0,0 +1,67 @@
+//Day 3 Function types - 1.Named function 2.Function Declaration 3.Anonymous Function 4.Function Expression
+//                        5.Arrow function 6.Higher Order Function 7.Callback Function
+
+// function addition(){
+
+//   console.log(2+2)
+// }
+// addition()
+
+// function add(a, b){  // a, b are parameters
+
+//   console.log(a + b)
+// }
+// add(4,5)  // 4,5 are argument
+// add(2,3)
+
+
+//~ function declaration
+// Hoisting is possible only with Function Declaration
+// add(6,7)
+// function add(a,b) { // the hoisting is possible
+//   console.log(a + b)
+// }
+
+
+//~ function expression
+// a(6,7)
+// var a = function(a,b) { // here we are using function expression, so hoisting not possible
+// console.log(a + b) 
+// }
+
+
+//~ Arrow function
+
+// let app =()=>{
+//     console.log("hello");
+    
+// }
+// app()
+
+
+// let app1 =_=>console.log("yoyo");
+// app1()
+
+// let app2 =a=>console.log("bolo");
+// app2()
+
+
+
+// let num = a=>{return a}
+// console.log(num(5));
+
+//~ higher order func
+
+// function add(a){
+//     return a()
+// }
+// console.log(add(num));
+
+//~ callback func
+
+// function num(){
+//     return 8
+// }
+
+
+
---
+++ scriptDay4.js
@@ -0,0 +1,155 @@
+//Day 4
+
+
+//Q.1 check whether eligible for voting or not
+
+// let age=prompt("Enter your age:")
+
+// function voting() {
+
+//     if(age>=18)
+//     {
+//         console.log("Eligible for Voting")
+//     }
+
+//     else{
+//         console.log("Not Eligible for Voting")
+//     }
+// }
+// voting()
+
+
+//Q2. check whether even or odd
+
+// let num = prompt("enter a number")
+
+// let Numcheck = () => {
+
+//     if(num%2==0)
+//     {
+//         console.log(num+ " is Even Number")
+//     }
+
+//     else{
+//         console.log (num+" is Odd Number")
+//     }
+// }
+// Numcheck()
+
+
+
+//Q3. addition of 2 numbers
+
+// let num1 = parseInt(prompt("Enter 1st Number:"))
+// let num2 = parseInt(prompt("Enter 2nd Number:"))
+
+// function addition() {
+//     return num1 + num2
+// }
+// console.log(addition())
+
+
+
+//Q.4 voting age Q but in a different way
+
+
+
+//  function voting(age) {
+
+//      if(age>=18)
+//      {
+//          console.log("Eligible for Voting")
+//      }
+
+//      else{
+//          console.log("Not Eligible for Voting")
+//      }
+//  }
+  //voting(20) //way1
+
+ //let a = parseInt(prompt("Enter your age:"))  //way2 
+ //voting(a)
+
+
+
+//& Q1.  create a function that accepts 2 numbers and return the largest number
+
+// let num1 = parseInt(prompt("Enter 1st Number:"))
+// let num2 = parseInt(prompt("Enter 2nd Number:"))
+
+// function Greater() {
+
+//     if(num1>num2)
+//     {
+//         console.log(num1 + " is Greater")
+//     }
+//     else{
+//         console.log(num2 + " is Greater")
+//     }
+// }
+// Greater()
+
+
+//& Q2. create a function that accepts a number and check whether the number is positive , negative or zero
+
+// let number = parseInt(prompt("Enter a Number:"))
+
+// function checkNumber() {
+
+//     if(number>0)
+//     {
+//         console.log(number + " is Positive Number")
+//     }
+//     else if(number<0){
+//         console.log(number + " is Negative Number")
+//     }
+//     else{
+//         console.log("The number is Zero")
+//     }
+// }
+// checkNumber()
+
+
+// //& Q3. create a function that check whether a year is leap year or not
+
+// let year = parseInt(prompt("Enter a Year:"))
+
+// function leapYear() {
+
+//     if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+//     {
+//         console.log(year + " is a Leap Year")
+//     }
+//     else{
+//         console.log(year + " is not a Leap Year")
+//     }
+// }
+// leapYear()
+
+
+// //& Q4. write a function that finds the largest of three numbers using only if...else
+
+// let num1 = parseInt(prompt("Enter 1st Number:"))
+// let num2 = parseInt(prompt("Enter 2nd Number:"))  
+// let num3 = parseInt(prompt("Enter 3rd Number:"))
+
+// function Largest() {
+
+//     if(num1>num2 && num1>num3){
+//         console.log(num1+ " is greatest")
+// }
+
+//     else if(num2>num3){
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
