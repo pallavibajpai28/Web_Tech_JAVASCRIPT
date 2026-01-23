896e036c708450764e91dc48776704b6f95cd634
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-01-23 12:50:35 +0530

    Update scriptDay2.js

--- scriptDay2.js
+++ scriptDay2.js
@@ -1,58 +1,58 @@
-//Day 2
+Day 2
 
-// //~ Declaration types -var
+//~ Declaration types -var
 
-// var a;
-// console.log(a)
+var a;
+console.log(a)
 
-// a=10;
-// console.log(a);
+a=10;
+console.log(a);
 
-// var b=20;
-// console.log(b);
+var b=20;
+console.log(b);
 
-// var a;
-// console.log(a);
-
-
-// a=20;
-// console.log(a);
-
-// //~let
-
-// let a;
-// console.log(a);
-
-// a=20;
-// console.log(a);
-
-// let b=30;
-// console.log(b);
-
-// a=40;
-// console.log(a);
-
-// //~ const
-
-// const d=20;
-// console.log(d);
+var a;
+console.log(a);
 
 
+a=20;
+console.log(a);
 
-//~ Type Conversion
+//~let
 
-// console.log("hello" + 10);
-// console.log("hello"+"string");
-// console.log("hello"+"10");
-// console.log(10+"10");
-// console.log(10+true);
-// console.log(10+10);
-// console.log(true+"10");
-// console.log(true+true);
-// console.log("30"-10);
-// console.log("hello"-10);
-// console.log(null -20);
-// console.log(10=="10");
-// console.log(10 ==="10");
-// console.log(true==1);
-// console.log(true===1);
+let a;
+console.log(a);
+
+a=20;
+console.log(a);
+
+let b=30;
+console.log(b);
+
+a=40;
+console.log(a);
+
+//~ const
+
+const d=20;
+console.log(d);
+
+
+
+~ Type Conversion
+
+console.log("hello" + 10);
+console.log("hello"+"string");
+console.log("hello"+"10");
+console.log(10+"10");
+console.log(10+true);
+console.log(10+10);
+console.log(true+"10");
+console.log(true+true);
+console.log("30"-10);
+console.log("hello"-10);
+console.log(null -20);
+console.log(10=="10");
+console.log(10 ==="10");
+console.log(true==1);
+console.log(true===1);
