96c13309ed0866fee90a15b897f9aec62a9600be
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-01-23 13:06:39 +0530

    Add files via upload

---
+++ scriptDay8.js
@@ -0,0 +1,44 @@
+//Day 8 
+
+//^ Object : collection of key value pairs 
+
+
+
+// let student = {
+//     name : "Pallavi",
+//     age : 22,
+//     isMarried : false,
+//     hobbies: ["Sleeping", "Coding", "singing"],
+//     isWorking : ()=>"Yes, I am Working",
+//     address : {city : "Vashi"}
+
+// }
+
+// console.log(student.name)
+// console.log(student.age)
+// console.log(student.isMarried)
+// console.log(student.hobbies)
+// console.log(student.isWorking())
+// console.log(student.address)
+// console.log(student.address.city)
+
+
+
+
+
+let employee = {
+    name : "Pallavi Bajpai",
+    age : 23,
+    email : "pallavibajpai28@gmail.com",
+    dept : "IT",
+    sal : "40000",
+}
+
+
+
+console.log(`Employee name is ${name}`)
+console.log(`Employee ${name} age is ${age}`)  
+console.log(`Employee's email id is ${email}`)                                   
+console.log(`Department is ${dept}`)
+console.log(`Employee's salary is ${sal}`)
+
