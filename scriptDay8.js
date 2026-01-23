75436e1455c619018b165368ea6bb22148f9a820
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-01-23 13:10:09 +0530

    Update scriptDay8.js

--- scriptDay8.js
+++ scriptDay8.js
@@ -1,34 +1,34 @@
-//Day 8 
+// Day 8 
 
-//^ Object : collection of key value pairs 
-
-
-
-// let student = {
-//     name : "Pallavi",
-//     age : 22,
-//     isMarried : false,
-//     hobbies: ["Sleeping", "Coding", "singing"],
-//     isWorking : ()=>"Yes, I am Working",
-//     address : {city : "Vashi"}
-
-// }
-
-// console.log(student.name)
-// console.log(student.age)
-// console.log(student.isMarried)
-// console.log(student.hobbies)
-// console.log(student.isWorking())
-// console.log(student.address)
-// console.log(student.address.city)
+// ^ Object : collection of key value pairs 
 
 
 
+let student = {
+    name : "Pallavi",
+    age : 22,
+    isMarried : false,
+    hobbies: ["Sleeping", "Coding", "singing"],
+    isWorking : ()=>"Yes, I am Working",
+    address : {city : "Vashi"}
 
+}
+
+console.log(student.name)
+console.log(student.age)
+console.log(student.isMarried)
+console.log(student.hobbies)
+console.log(student.isWorking())
+console.log(student.address)
+console.log(student.address.city)
+
+
+
+// extarction of values from object (destructuring): destructuring means pulling out values from object and storing them in variables using ${}
 
 let employee = {
-    name : "Pallavi Bajpai",
-    age : 23,
+    name1 : "Pallavi Bajpai",
+    age1 : 23,
     email : "pallavibajpai28@gmail.com",
     dept : "IT",
     sal : "40000",
@@ -36,9 +36,10 @@ let employee = {
 
 
 
-console.log(`Employee name is ${name}`)
-console.log(`Employee ${name} age is ${age}`)  
+console.log(`Employee name is ${name1}`)
+console.log(`Employee ${name1} age is ${age1}`)  
 console.log(`Employee's email id is ${email}`)                                   
 console.log(`Department is ${dept}`)
 console.log(`Employee's salary is ${sal}`)
+
 
