7bdb0d93a24a7f1e004ba3d4f7657180efff3d20
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-01-30 13:41:25 +0530

    Update scriptDay12.js

--- scriptDay12.js
+++ scriptDay12.js
@@ -1,40 +1,41 @@
-// //Day 12
+//Day 12
 
 
-// //^ Asynchronous JavaScript : it allows the program to perform tasks without blocking the main thread, enabling other operations to continue while waiting for certain tasks to complete, such as fetching data from a server or waiting for user input.
+//^ Asynchronous JavaScript : it allows the program to perform tasks without blocking the main thread, enabling other operations to continue while waiting for certain tasks to complete, such as fetching data from a server or waiting for user input.
 
-// //* Common Asynchronous Methods in JavaScript :
+//* Common Asynchronous Methods in JavaScript :
 
-// //! setTimeout() : This function is used to execute a specified function or code snippet after a specified delay (in milliseconds).
+//! setTimeout() : This function is used to execute a specified function or code snippet after a specified delay (in milliseconds).
 
-// //! setInterval() : This function is used to execute a specified function or code snippet repeatedly at specified intervals (in milliseconds).
+//! setInterval() : This function is used to execute a specified function or code snippet repeatedly at specified intervals (in milliseconds).
 
-// //! clearInterval() : This function is used to stop the execution of a function that was set to run repeatedly using setInterval().
+//! clearInterval() : This function is used to stop the execution of a function that was set to run repeatedly using setInterval().
 
-// console.log("1st line")
-// console.log("2nd line")
-// console.log("imp")
-// console.log("1st line")
-// setTimeout(()=>{
-//         console.log("2nd line")
-//     },1000);
+console.log("1st line")
+console.log("2nd line")
+console.log("imp")
+console.log("1st line")
+setTimeout(()=>{
+        console.log("2nd line")
+    },1000);
     
-//     console.log("imp");
-// console.log("hii")
-// setInterval(()=>{
-//     console.log("i am executing again and again")
-// },1500)
+    console.log("imp");
+console.log("hii")
+setInterval(()=>{
+    console.log("i am executing again and again")
+},1500)
 
-// setInterval
-// setTimeout
+setInterval
+setTimeout
 
-// let count  = 0 
-// let IntervalId = setInterval(()=>{
-//     count++
-//     console.log(count)
+let count  = 0 
+let IntervalId = setInterval(()=>{
+    count++
+    console.log(count)
 
-//     if(count === 10){
-//         clearInterval(IntervalId)
-//         console.log("Done....")
-//     }
-// },1000)
+    if(count === 10){
+        clearInterval(IntervalId)
+        console.log("Done....")
+    }
+
+},1000)
